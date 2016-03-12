7027f53148377ca7463c47441d927a7c3cbc82a5
jeff <muelljef@github.com>
2016-03-12 18:51:09 +1000

    delete trip functionality working

--- app/src/main/java/com/example/jeffrey/travellingturtle/TripActivity.java
+++ app/src/main/java/com/example/jeffrey/travellingturtle/TripActivity.java
@@ -33,9 +33,22 @@ public class TripActivity extends AppCompatActivity {
 
     public void deleteTrip(View view) {
 
+        DeleteTripTask deleteTripTask = new DeleteTripTask();
+        String url = "http://54.206.28.148/api/v1/trips/";
+        try {
+            url += URLEncoder.encode(tripId, "UTF-8");
+            url += "?password=" + URLEncoder.encode(password, "UTF-8");
+            url += "&username=" + URLEncoder.encode(username, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        deleteTripTask.execute(url);
+
     }
 
     public void updateTripPage(View view) {
+
+        //Intent updateIntent = new Intent(getApplicationContext(), );
 
     }
 
@@ -133,6 +146,70 @@ public class TripActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
+        }
+    }
+
+    public class DeleteTripTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            String charset = "UTF-8";
+
+            try {
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("DELETE");
+
+                int status = urlConnection.getResponseCode();
+                InputStream in;
+                if (status >= 400) {
+                    in = urlConnection.getErrorStream();
+                } else {
+                    in = urlConnection.getInputStream();
+                }
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                return result;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            if(result != null) {
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+                    //FAILURE
+                    if(jsonObject.has("error")) {
+                        Toast.makeText(TripActivity.this, "Sorry an error occurred", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    //SUCCESS
+                    finish();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
